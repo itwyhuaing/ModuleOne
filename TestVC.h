6efe28007cc7a1eaf382926dc48100cfb62acb19
hnbwyh <rainwang@hinabian.com>
2018-03-27 17:58:16 +0800

    新增文件并修改

---
+++ TestVC.h
@@ -0,0 +1,13 @@
+//
+//  TestVC.h
+//  ProjectADemo
+//
+//  Created by hnbwyh on 2018/3/27.
+//  Copyright © 2018年 ZhiXingJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestVC : UIViewController
+
+@end
---
+++ TestVC.m
@@ -0,0 +1,37 @@
+//
+//  TestVC.m
+//  ProjectADemo
+//
+//  Created by hnbwyh on 2018/3/27.
+//  Copyright © 2018年 ZhiXingJY. All rights reserved.
+//
+
+#import "TestVC.h"
+
+@interface TestVC ()
+
+@end
+
+@implementation TestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@" \n \n %s \n \n ",__FUNCTION__);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
